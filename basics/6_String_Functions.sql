1710b1dd6ccbdb46dc4892f1b381518957058e7b
diabennd <aidulbeni@gmail.com>
2025-12-30 22:19:00 +0700

    update 6_String_Functions.sql

--- basics/6_String_Functions.sql
+++ basics/6_String_Functions.sql
@@ -1,21 +1,33 @@
 -- LENGTH
+SELECT 
+    LENGTH('Benny Way') -- 9
 
 -- LEFT, RIGHT 
+SELECT 
+    LEFT('Benny Way', 3), -- Returns -> 'Ben'
+    RIGHT('Benny Way', 3) -- Returns -> 'Way'
 
 
--- SUBSTRING
+-- SUBSTRING => Extracts a part of a string, given a starting position and length.
+SELECT 
+    SUBSTRING('Benny Way', 1, 5) -- Returns -> 'Benny'
 
 
--- LOCATE
+-- REPLACE => Replaces all occurrences of a substring within a string.
+SELECT 
+    REPLACE('Benny Way', 'Way', 'Arthur')
 
 
--- REPLACE
+-- TRIM => Removes leading and trailing spaces (or other specified characters).
+SELECT 
+    TRIM('  Benny   ')
 
--- TRIM
+
 
 -- CONCAT
-SELECT first_name, last_name, 
-CONCAT(first_name, ' ', last_name) full_name
+SELECT 
+    first_name, last_name, 
+    CONCAT(first_name, ' ', last_name) AS full_name
 FROM employee_demographics;
 
 
