d7d3e3e0e31e607a6443b0ae6789517de22b4e72
diabennd <aidulbeni@gmail.com>
2025-12-30 22:17:46 +0700

    update 4_Joins.sql

--- basics/4_Joins.sql
+++ basics/4_Joins.sql
@@ -1,11 +1,8 @@
-SELECT * 
-FROM parks_and_recreation.employee_demographics;
-
 -- Inner Join
 SELECT * 
-FROM employee_demographics AS dem
-INNER JOIN employee_salary AS sal
-	ON sal.employee_id = dem.employee_id;
+FROM job_postings_fact AS jf
+INNER JOIN company_dim AS cd
+	ON jf.company_id = cd.company_id;
 
 -- Outer Join
 SELECT * FROM employee_demographics as dem
@@ -14,10 +11,22 @@ RIGHT OUTER JOIN employee_salary as sal
 ;
 
 -- Self Join
+SELECT * 
+FROM job_postings_fact AS jf
+JOIN job_postings_fact AS jf2
+	ON jf.job_id = jf2.job_id;
 
 
 -- Joining Multiple Tables
-
+SELECT 
+	skills,
+	count(sjob_dim) AS num_of_jobs, 
+	ROUND(AVG(job_posts.salary_year_avg), 2) AS average_salary
+FROM job_postings_fact AS job_posts
+LEFT JOIN skills_job_dim AS sjob_dim
+	ON job_posts.job_id = sjob_dim.job_id
+LEFT JOIN skills_dim AS sd
+	ON sjob_dim.skill_id = sd.skill_id
 
 
 
